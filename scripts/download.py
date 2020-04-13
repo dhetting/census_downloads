3d04bec4d49e8a01ca7bfc6523118c9aeee1c247
EC2 Default User <ec2-user@ip-172-31-21-59.us-west-2.compute.internal>
2020-04-13 21:31:38 +0000

    python import nonsense

--- scripts/download.py
+++ scripts/download.py
@@ -1,8 +1,18 @@
 import os
 import sys
 
-from src.cdown import Cdown
+# Refer: https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
+# Refer: https://stackoverflow.com/questions/4383571/importing-files-from-different-folder
+import importlib, importlib.util
 
+def module_from_file(module_name, file_path):
+    spec = importlib.util.spec_from_file_location(module_name, file_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return module
+
+cdown = module_from_file("cdown", "../src/cdown/__init__.py")
+ 
 if __name__ == '__main__':
     year = sys.argv[1]
     var = sys.argv[2]
@@ -12,6 +22,6 @@ if __name__ == '__main__':
     except:
         ws = os.path.join(os.curdir, year)
 
-    cd = Cdown(year=year, ws=ws)
+    cd = cdown.Cdown(year=year, ws=ws)
 
     cd.get(var)
